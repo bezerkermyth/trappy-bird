fc6ef19621264980a4831876945e91424af5e749
theImmortalMonkey <noneofyourbusiness@gmmmmmail.com>
2018-08-05 16:37:35 +0700

    added reset (on main camera)

---
+++ Assets/Scripts/SimpleReset.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleReset : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public GameObject target;
+
+    // Update is called once per frame
+    void Update () {
+		        if (!target)
+        {
+            Application.LoadLevel("SampleScene");
+        }
+	}
+
+
+    
+
+
+    //void Reset()
+    //{
+    //    //Output the message to the Console
+    //    Debug.Log("Reset");
+    //    if (!target)
+    //        target = GameObject.FindWithTag("Player");
+    //}
+}
+
