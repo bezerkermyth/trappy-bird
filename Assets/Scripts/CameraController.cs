138649b5008e67ecf00ffa8d1da9b4684fb50420
Furqaan <mohamad.furqaan@gmail.com>
2018-08-05 13:41:02 +0800

    initial commit to github

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    public float speed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        //    if (Input.GetKey(KeyCode.LeftArrow))
+        //    {
+        //        Vector3 position = this.transform.position;
+        //        position.x = position.x -0.3f;
+        //        this.transform.position = position;
+        //    }
+        //    else if (Input.GetKey(KeyCode.RightArrow))
+        //    {
+        //        Vector3 position = this.transform.position;
+        //        position.x = position.x + 0.3f;
+        //        this.transform.position = position;
+        //    }
+        Vector3 position = this.transform.position;
+        position.x = position.x + speed;
+        this.transform.position = position;
+}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerControllerVanilla : PhysicsObject
+{
+    
+    public float maxSpeed = 7;
+    public float jumpTakeOffSpeed = 7;
+
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
+
+    private GameObject bgCamera;
+
+    private float bgSpeed;
+
+    // Use this for initialization
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+
+
+        CameraController cmct = Camera.main.GetComponent<CameraController>();
+        bgSpeed = cmct.speed;
+    }
+
+    protected override void ComputeVelocity()
+    {
+        Vector2 move = Vector2.zero;
+
+        move.x = Input.GetAxis("Horizontal");
+        move.x = bgSpeed*10;
+
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            velocity.y = jumpTakeOffSpeed;
+        }
+        else if (Input.GetButtonUp("Jump"))
+        {
+            if (velocity.y > 0)
+            {
+                velocity.y = velocity.y * 0.5f;
+            }
+        }
+
+        bool flipSprite = (spriteRenderer.flipX ? (move.x > 0.01f) : (move.x < 0.01f));
+        if (flipSprite)
+        {
+            spriteRenderer.flipX = !spriteRenderer.flipX;
+        }
+
+        animator.SetBool("grounded", grounded);
+        animator.SetFloat("velocityX", Mathf.Abs(velocity.x) / maxSpeed);
+
+        targetVelocity = move * maxSpeed;
+    }
+}
