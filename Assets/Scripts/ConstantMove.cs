678b97485736cc6e41e6841f61798930b8b8975e
stellarLuminant <stellarLuminant@gmail.com>
2018-08-07 20:48:30 -0700

    Functioning Main Menu
    - A very simple main menu. In order to test it, you must load MainMenu as your active scene and hit play.

---
+++ Assets/Scripts/ConstantMove.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class ConstantMove : MonoBehaviour 
+{
+    [SerializeField]
+    private float _unitsPerSecond = 1;
+
+    [SerializeField]
+    private Vector2 _direction = Vector2.right;
+
+	void Update ()
+    {
+        transform.Translate (_direction * _unitsPerSecond * Time.deltaTime);
+	}
+}
