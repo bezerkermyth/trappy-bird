a9a8fe2c9599e5ace9976f3d2f2b2187a8e019bc
Furqaan <mohamad.furqaan@gmail.com>
2018-08-10 00:26:28 +0800

    added level complete trigger, level complete menu

---
+++ Assets/LevelComplete.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class LevelComplete : MonoBehaviour {
+
+	private void OnTriggerEnter2D(Collider2D collision)
+    {
+        FindObjectOfType<LevelCompleteMenu>().Pause();
+
+        
+    }
+}
+
