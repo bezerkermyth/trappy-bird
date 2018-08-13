9e5dc0ac3aa6ff910f6168418d0db69c1c8797b4
stellarLuminant <stellarLuminant@gmail.com>
2018-08-13 09:22:53 -0700

    Player collision fix and UI Scaling
    - Players can no longer stand on killing blocks without getting hurt.
    - UI now scales to larger screens with 1024x768 as a reference resolution.

--- Assets/Scripts/Gameplay/DamagePlayerOnCollision.cs
+++ Assets/Scripts/Gameplay/DamagePlayerOnCollision.cs
@@ -2,14 +2,13 @@
 
 public class DamagePlayerOnCollision : MonoBehaviour
 {
-    [SerializeField]
-    private int _damage = 1;
+    public int Damage = 1;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         PlayerController controller = collision.gameObject.GetComponent<PlayerController> ();
 
         if (controller != null)
-            controller.TakeDamage (_damage);
+            controller.TakeDamage (Damage);
     }
 }
