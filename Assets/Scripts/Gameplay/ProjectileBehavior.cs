e5a1abeb227897c2b814da35923d2776d6e9a0e3
Ephesius <jnhardin85@gmail.com>
2018-08-17 20:51:31 -0500

    Added a trip wire trap

---
+++ Assets/Scripts/Gameplay/ProjectileBehavior.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class ProjectileBehavior : MonoBehaviour
+{
+    public float Speed;
+
+    private Transform _playerTransform;
+
+    private void Start()
+    {
+        _playerTransform = GameObject.Find("Player").transform;
+    }
+
+    private void FixedUpdate()
+    {
+        transform.Translate(Vector3.left.normalized * Speed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, _playerTransform.position) > 40f)
+            Destroy(this.gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        PlayerController controller = collision.gameObject.GetComponent<PlayerController>();
+
+        if (controller != null)
+            controller.Die();
+    }
+}
---
+++ Assets/Scripts/Gameplay/TripWire.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class TripWire : MonoBehaviour
+{
+    public Transform PlayerTransform;
+    public GameObject Projectile;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        PlayerController _controller = collision.gameObject.GetComponent<PlayerController>();
+
+        if (_controller != null)
+        {
+            SpawnProjectile(Projectile);
+            this.gameObject.SetActive(false);
+        }
+    }
+
+    // Spawns projectile, behavior of projectile once spawned is handled by ProjectileBehavior script
+    void SpawnProjectile(GameObject _projectile)
+    {
+        Instantiate(_projectile, 
+                    new Vector3(PlayerTransform.position.x + 30f,
+                                PlayerTransform.position.y,
+                                0f),
+                    Quaternion.identity);
+    }
+}
